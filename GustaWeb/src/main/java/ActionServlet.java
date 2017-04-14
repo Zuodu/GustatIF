efeb6e0442fc104c4ee379771337ada010c8a0a7
zifanYao <zuoduzuodu24@gmail.com>
2017-04-15 00:06:10 +0200

    success
    servlet success on tomcat 7

--- GustaWeb/src/main/java/ActionServlet.java
+++ GustaWeb/src/main/java/ActionServlet.java
@@ -6,10 +6,19 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import dao.JpaUtil;
+import metier.modele.Restaurant;
+import metier.service.*;
 
 
 /**
@@ -17,6 +26,18 @@ import javax.servlet.http.HttpServletResponse;
  * @author zifanYao
  */
 public class ActionServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        JpaUtil.init();
+    }
+
+    @Override
+    public void destroy() {
+        JpaUtil.destroy();
+        super.destroy();
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,11 +50,26 @@ public class ActionServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        response.
+        //init des services metiers et autre
+        ServiceMetier metier = new ServiceMetier();
+
+        String methode = request.getMethod();
+        String action = request.getParameter("action");
+        if(action.equals("recupererListeRestaurants")){
+            List<Restaurant> restaurants = null;
+            try{
+                restaurants = metier.recupererListeRestaurants();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            PrintWriter pw = response.getWriter();
+            printListeRestaurants(pw,restaurants);
+            pw.close();
+        }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -72,4 +108,21 @@ public class ActionServlet extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private void printListeRestaurants(PrintWriter pw,List<Restaurant> liste)
+    {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonArray jsonliste = new JsonArray();
+        for(Restaurant r: liste)
+        {
+            JsonObject jsonRestaurant = new JsonObject();
+            jsonRestaurant.addProperty("id",r.getId());
+            jsonRestaurant.addProperty("denomination",r.getDenomination());
+            jsonRestaurant.addProperty("description",r.getDescription());
+            jsonRestaurant.addProperty("adresse",r.getAdresse());
+            jsonliste.add(jsonRestaurant);
+        }
+        JsonObject container = new JsonObject();
+        container.add("restaurants",jsonliste);
+        pw.println(gson.toJson(container));
+    }
 }
