7d338d3a56dcb6e621276a3c70813cec6f621c2d
zuoduzuodu <zuoduzuodu24@gmail.com>
2017-04-15 18:37:19 +0200

    starting dev on mac : success, but maybe problems on window ?

--- GustaNoyau/target/generated-sources/annotations/metier/modele/Restaurant_.java
+++ GustaNoyau/target/generated-sources/annotations/metier/modele/Restaurant_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import metier.modele.Produit;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-14T18:14:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-15T18:27:34")
 @StaticMetamodel(Restaurant.class)
 public class Restaurant_ { 
 
